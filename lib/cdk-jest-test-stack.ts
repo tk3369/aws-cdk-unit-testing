3e0ad4022cdc604e0dcca1e0450ffeb3cc8589f4
Kwong, Thomas K <Thomas.Kwong@westernasset.com>
2019-12-03 12:40:56 -0800

    first commit

--- lib/cdk-jest-test-stack.ts
+++ lib/cdk-jest-test-stack.ts
@@ -1,9 +1,14 @@
 import cdk = require('@aws-cdk/core');
+import s3 = require('@aws-cdk/aws-s3');
 
 export class CdkJestTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new s3.Bucket(this, 'test-bucket', {
+      bucketName: 'test-bucket',
+      versioned: true
+    });
   }
 }
--- test/cdk-jest-test.test.ts
+++ test/cdk-jest-test.test.ts
@@ -1,13 +1,69 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, beASupersetOfTemplate, exactlyMatchTemplate } from '@aws-cdk/assert';
 import cdk = require('@aws-cdk/core');
 import CdkJestTest = require('../lib/cdk-jest-test-stack');
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new CdkJestTest.CdkJestTestStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test('exact match (case 1)', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkJestTest.CdkJestTestStack(app, 'MyTestStack');
+  
+  // Case 1 - exact match
+  expectCDK(stack).to(exactlyMatchTemplate({
+    "Resources": {
+      "testbucketE6E05ABE": {
+        "Type": "AWS::S3::Bucket",
+        "Properties": {
+          "BucketName": "test-bucket",
+          "VersioningConfiguration": {
+            "Status": "Enabled"
+          }
+        },
+        "UpdateReplacePolicy": "Retain",
+        "DeletionPolicy": "Retain"
+      }
+    }
+  }))
+});
+
+test('superset match (case 2)', () => {
+  const app = new cdk.App();
+  const stack = new CdkJestTest.CdkJestTestStack(app, 'MyTestStack');
+  expectCDK(stack).to(beASupersetOfTemplate({
+    "Resources": {
+      "testbucketE6E05ABE": {
+        "Type": "AWS::S3::Bucket",
+        "Properties": {
+          "BucketName": "test-bucket",
+          "VersioningConfiguration": {
+            "Status": "Enabled"
+          }
+        },
+        "UpdateReplacePolicy": "Retain",
+        // XXX - commented out the following... should work?
+        // "DeletionPolicy": "Retain"
+      }
+    }
+  }))
+})
+
+test('superset match (case 3)', () => {
+  const app = new cdk.App();
+  const stack = new CdkJestTest.CdkJestTestStack(app, 'MyTestStack');
+  expectCDK(stack).to(beASupersetOfTemplate({
+    "Resources": {
+      "testbucketE6E05ABE": {
+        "Type": "AWS::S3::Bucket",
+        "Properties": {
+          "BucketName": "test-bucket",
+          "VersioningConfiguration": {
+            "Status": "Enabled"
+          }
+        },
+        "UpdateReplacePolicy": "Retain",
+        "DeletionPolicy": "Retain",
+        // addd new attribute not from the stack definition -- should fail?
+        "ObjectLockEnabled" : true,   
+      }
+    }
+  }))
 });
